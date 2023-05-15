dc1d4ce4c6a1c59f0e574330a34fefe7010c79bf
Sujal Chordia <sujalchordia10@gmail.com>
2023-05-16 04:26:58 +0530

    deployment issues

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 //middleware
 app.use((req,res,next)=>{
-res.setHeader("Access-Control-Allow-Origin","*");
+res.setHeader("Access-Control-Allow-Origin","https://farmeasyyy.onrender.com");
 res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Control-Type, Accept"
